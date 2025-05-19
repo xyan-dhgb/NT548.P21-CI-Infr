f3e4913211e778d8d2d95c6e40dcdeb8fe4a4f2f
Đinh Huỳnh Gia Bảo <giabaoctg@gmail.com>
2025-05-19 20:31:37 +0700

    update(jenkins-setup): add apt wait until system is not locked anymore

--- modules/jenkins/jenkins-setup.sh
+++ modules/jenkins/jenkins-setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+while sudo fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
+    echo "Waiting for apt lock to be released..."
+    sleep 5
+done
+
 # Cập nhật hệ thống
-apt-get update -y
-apt-get install -y openjdk-17-jdk curl gnupg2
+sudo apt-get update -y
+sudo apt-get install -y openjdk-17-jdk curl gnupg2
 
 # Thêm khóa Jenkins
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | tee \
+sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
     /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 # Thêm Jenkins repo
@@ -14,9 +19,9 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 # Cài Jenkins
-apt-get update -y
-apt-get install -y jenkins
+sudo apt-get update -y
+sudo apt-get install -y jenkins
 
 # Khởi động Jenkins
-systemctl enable jenkins
-systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
